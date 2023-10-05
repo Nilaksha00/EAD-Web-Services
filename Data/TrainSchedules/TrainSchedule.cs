b7f9b5a22dfa7f20613734e0e9da30d9773a7d74
Praveen Kariyawasam <praveenkariyawasam0@gmail.com>
2023-10-05 13:48:45 +0530

    Varianles updated

--- Data/TrainSchedules/TrainSchedule.cs
+++ Data/TrainSchedules/TrainSchedule.cs
@@ -6,11 +6,11 @@ namespace EAD_Project.Data.TrainSchedules
     {
         [BsonId]
         // BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
-        public string trainScheduleID { get; set; }
-        public string trainScheduleDate { get; set; }
-        public string trainScheduleDept { get; set; }
-        public string trainScheduleArr { get; set; }
-        public string trainScheduleTrainID { get; set; }
+        public string? trainScheduleID { get; set; }
+        public string? trainScheduleDate { get; set; }
+        public string? trainScheduleDept { get; set; }
+        public string? trainScheduleArr { get; set; }
+        public string? trainScheduleTrainID { get; set; }
 
     }
 }
